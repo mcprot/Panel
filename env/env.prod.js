068ac4d6565587b84ef1fb96c12eb8661b61ec90
yonderbread <realmctbf@gmail.com>
2021-01-31 02:37:39 -0800

    :sparkles: Clean up branch

--- env/env.prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    PORT: process.env.PORT,
-    MONGO: process.env.MONGO,
-    WORKER: process.env.WORKER,
-    KEY: process.env.KEY
-}
--- routes/docs.route.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-router.get('/faq', function (req, res, next) {
-    return res.render("docs_faq", {title: 'Docs | FAQ'});
-});
-
-module.exports = router;
